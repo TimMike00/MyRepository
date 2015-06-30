a444739ae7c6e9da6f5e86d922bd8ccb62bb0f52
TimMike <tim.mike00@yandex.ua>
2015-06-30 12:58:53 +0300

    all files

---
+++ lol lol/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Empty
+//
+//  Created by Petro Rospopa on 02.03.15.
+//  Copyright (c) 2015 PetroRospopa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
